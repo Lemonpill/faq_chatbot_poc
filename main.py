7418dcc30aa1f98390867730d2fae1172792d504
Valeri <code.lemonpill@gmail.com>
2026-03-18 21:36:54 +0200

    usage cost

--- main.py
+++ main.py
@@ -5,7 +5,88 @@ import sounddevice as sd
 import scipy.io.wavfile as wav
 from typing import List, Dict
 from openai import OpenAI
+from openai.types.create_embedding_response import Usage
+from openai.types.completion_usage import CompletionUsage
 from dotenv import load_dotenv
+from mutagen.mp3 import MP3
+import wave
+
+EMBED_MODEL = "text-embedding-3-small"
+AUDIO_MODEL = "gpt-4o-mini-tts"
+TEXT_MODEL = "gpt-4.1-nano"
+TRANS_MODEL = "gpt-4o-mini-transcribe"
+
+
+MODEL_PRICE = {  # USD / Per 1M tokens
+    EMBED_MODEL: {
+        "Cost": 0.02,
+        "Batch cost": 0.01,
+    },
+    AUDIO_MODEL: {  # Audio
+        "Input": None,
+        "Output": 12.00,
+        "Estimated cost": 0.015,  # / minute
+    },
+    TEXT_MODEL: {
+        "Input": 0.10,
+        "Cached input": 0.025,
+        "Output": 0.40,
+    },
+    TRANS_MODEL: {
+        "Input": 1.25,
+        "Output": 5.00,
+        "Estimated cost": 0.003,  # / minute
+    },
+}
+
+
+def get_mp3_duration(filename: str):
+    audio = MP3(filename)
+    return audio.info.length
+
+
+def get_wav_duration(filename: str):
+    duration_seconds = None
+    with wave.open(filename, "rb") as wf:
+        frames = wf.getnframes()
+        rate = wf.getframerate()
+        duration_seconds = frames / float(rate)
+        print(f"Audio duration: {duration_seconds:.2f} seconds")
+    return duration_seconds
+
+
+def get_usage_cost_embedding(usage: Usage):
+    costs = MODEL_PRICE[EMBED_MODEL]
+    cost_per_million = costs["Cost"]  # USD per 1M tokens
+    cost = cost_per_million * usage.total_tokens / 1000000
+    print(f"get_usage_cost_embedding: total ${cost:.6f}")
+    return cost
+
+
+def get_usage_cost_speak(duration: float):
+    minutes = duration / 60
+    cost = MODEL_PRICE[AUDIO_MODEL]["Estimated cost"] * minutes
+    print(f"get_usage_cost_speak: duration {duration}; cost {cost}")
+    return cost
+
+
+def get_usage_cost_text(usage: CompletionUsage):
+    tokens_in = usage.prompt_tokens
+    tokens_out = usage.completion_tokens
+    cost_in = MODEL_PRICE[TEXT_MODEL]["Input"]
+    cost_out = MODEL_PRICE[TEXT_MODEL]["Output"]
+    total_in = cost_in * tokens_in / 1000000
+    total_out = cost_out * tokens_out / 1000000
+    cost = total_in + total_out
+    print(f"get_usage_cost_text: total {cost} (in {total_in}; out {total_out})")
+    return cost
+
+
+def get_usage_cost_transcribe(duration: float):
+    minutes = duration / 60
+    cost = MODEL_PRICE[TRANS_MODEL]["Estimated cost"] * minutes
+    print(f"get_usage_cost_transcribe: duration {duration}; cost {cost}")
+    return cost
 
 
 # -----------------------
@@ -33,6 +114,7 @@ DATA = {
 # -----------------------
 def embed(text: str) -> List[float]:
     res = client.embeddings.create(model="text-embedding-3-small", input=text)
+    get_usage_cost_embedding(res.usage)
     return res.data[0].embedding
 
 
@@ -114,10 +196,13 @@ User question: {query}
 """
 
     res = client.chat.completions.create(
-        model="gpt-4.1-mini",
+        model="gpt-4.1-nano",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.2,
     )
+
+    usage = res.usage
+    get_usage_cost_text(usage)
 
     return res.choices[0].message.content.strip()
 
@@ -127,10 +212,10 @@ User question: {query}
 # -----------------------
 def record_audio(filename="input.wav", duration=5, fs=16000):
     print("🎤 Recording...")
-    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype="int16")  # ✅ int16
     sd.wait()
     wav.write(filename, fs, recording)
-    print("✅ Recording saved\n")
+    print(f"✅ Recording saved ({duration}s) to {filename}")
 
 
 # -----------------------
@@ -139,6 +224,8 @@ def record_audio(filename="input.wav", duration=5, fs=16000):
 def transcribe(filename="input.wav"):
     with open(filename, "rb") as f:
         res = client.audio.transcriptions.create(model="gpt-4o-mini-transcribe", file=f)
+    duration = get_wav_duration(filename)
+    get_usage_cost_transcribe(duration)
     return res.text
 
 
@@ -147,10 +234,10 @@ def transcribe(filename="input.wav"):
 # -----------------------
 def speak(text, filename="output.mp3"):
     res = client.audio.speech.create(model="gpt-4o-mini-tts", voice="alloy", input=text)
-
     with open(filename, "wb") as f:
         f.write(res.read())
-
+    duration_out = get_mp3_duration(filename)
+    get_usage_cost_speak(duration_out)
     print("🔊 Response saved to output.mp3\n")
 
 
